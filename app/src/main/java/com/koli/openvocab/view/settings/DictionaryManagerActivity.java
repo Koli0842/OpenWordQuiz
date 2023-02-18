b7fa5150c052a969336de4cd9be1a2f974064606
Dávid Kollár <kollardavid.kolibry@gmail.com>
2023-02-18 17:34:07 +0100

    Gradle 8, SDK build updates

--- app/src/main/java/com/koli/openvocab/view/settings/DictionaryManagerActivity.java
+++ app/src/main/java/com/koli/openvocab/view/settings/DictionaryManagerActivity.java
@@ -67,7 +67,7 @@ public class DictionaryManagerActivity extends AppCompatActivity {
     }
 
     private void launchImportDictionaryFlow() {
-        importDictionaryFlowLauncher.launch("*/*");
+        importDictionaryFlowLauncher.launch("application/json");
     }
 
     private void importDictionaryAndRequireConfirm(Uri fileUri) {
